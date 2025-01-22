a8ba4a55337d295e457c5992953c3173099250ec
ayalachn <81107065+ayalachn@users.noreply.github.com>
2025-01-22 20:18:52 +0200

    Update unitTestClockImageTime.py

--- unit_test/unitTestClockImageTime.py
+++ unit_test/unitTestClockImageTime.py
@@ -20,22 +20,96 @@ class unitTestClockImageTime(unittest.TestCase):
         self.converter = ClockImageTimeConverter()  # Create an instance of the ClockImageTimeConverter class
         self.project_path='../clock_time_coverter_project/'
     
-    def test_clock_converter(self):
+    def test_clock_converter_sanity(self):
         hour=1
         minute=30
-        second=9
         
         # create analog clock image
-        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute, second=second)
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
         
         # check result
-        hour_, minute_, second_ = self.converter.get_time_from_clock_image(img_result_path)
+        hour_, minute_ = self.converter.get_time_from_clock_image(img_result_path)
 
         self.assertEqual(hour, hour_)
-        self.assertEqual(minute, minute_)
-        self.assertEqual(second, second_)
+        self.assertAlmostEqual(minute, minute_, delta=1)
         
+    def test_clock_converter_same_angle(self):
+        hour=12
+        minute=0
+        
+        # create analog clock image
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
+        
+        # check result
+        hour_, minute_= self.converter.get_time_from_clock_image(img_result_path)
 
+        self.assertEqual(hour, hour_)
+        self.assertAlmostEqual(minute, minute_, delta=1)
+
+    def test_clock_converter_1st_quarter(self):
+        hour=4
+        minute=2
+        
+        # create analog clock image
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
+        
+        # check result
+        hour_, minute_= self.converter.get_time_from_clock_image(img_result_path)
+
+        self.assertEqual(hour, hour_)
+        self.assertAlmostEqual(minute, minute_, delta=1)        
+        
+    def test_clock_converter_2nd_quarter(self):
+        hour=8
+        minute=50
+        
+        # create analog clock image
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
+        
+        # check result
+        hour_, minute_= self.converter.get_time_from_clock_image(img_result_path)
+
+        self.assertEqual(hour, hour_)
+        self.assertAlmostEqual(minute, minute_, delta=1)      
+
+    def test_clock_converter_3th_quarter(self):
+        hour=2
+        minute=0
+        
+        # create analog clock image
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
+        
+        # check result
+        hour_, minute_= self.converter.get_time_from_clock_image(img_result_path)
+
+        self.assertEqual(hour, hour_)
+        self.assertAlmostEqual(minute, minute_, delta=1)   
+          
+    def test_clock_converter_4th_quarter(self):
+        hour=10
+        minute=42
+        
+        # create analog clock image
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
+        
+        # check result
+        hour_, minute_= self.converter.get_time_from_clock_image(img_result_path)
+
+        self.assertEqual(hour, hour_)
+        self.assertAlmostEqual(minute, minute_, delta=1)
+
+    def test_clock_converter_4th_quarter(self):
+        hour=10
+        minute=42
+
+        # create analog clock image
+        img_result_path = self.converter.draw_analog_clock(hour=hour, minute=minute)
+        
+        # check result
+        hour_, minute_= self.converter.get_time_from_clock_image(img_result_path)
+
+        self.assertEqual(hour, hour_)
+        self.assertAlmostEqual(minute, minute_, delta=1)
 if __name__ == "__main__":
     unittest.main()
     
